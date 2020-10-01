d7504b64601904a7cba3e166c91a5702b2e08ac8
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-10-01 06:02:11 +0200

    added static menu

--- sala.py
+++ sala.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import datetime
+import webbrowser
 from tkinter import *
 from tkinter import messagebox
 
@@ -19,6 +20,69 @@ main.iconbitmap("am6-logo.ico") # icon
 main.geometry('300x300') # area
 # Make the window unresizable
 main.resizable(0, 0)
+
+# Contact Us Menu opening functions
+
+def opening_fb():
+    webbrowser.open_new('facebook.com/AM6Page')
+
+def opening_website():
+    webbrowser.open_new('http://am6.tech/')
+
+def opening_whatsapp():
+    webbrowser.open_new('https://web.WhatsApp.com/send?phone=201553057088')
+
+def opening_telegram():
+    webbrowser.open_new('https://t.me/AM6SoftwareCom')
+
+def calling_us():
+    webbrowser.open_new('tel:+201553057088')
+
+# Menu
+sala_menu = Menu(main)
+main.config(menu=sala_menu)
+
+# File Menu
+file_menu = Menu(sala_menu, tearoff=False)
+sala_menu.add_cascade(label='File', menu=file_menu)
+file_menu.add_command(label="Exit", command=main.quit)
+
+# Edit Menu
+edit_menu = Menu(sala_menu, tearoff=False)
+sala_menu.add_cascade(label='Edit', menu=edit_menu)
+edit_menu.add_command(label='Yeasterday', command=None)
+edit_menu.add_command(label='Pick a day', command=None)
+
+# View Menu
+view_menu = Menu(sala_menu, tearoff=False)
+sala_menu.add_cascade(label='View', menu=view_menu)
+view_menu.add_command(label='Yeasterday', command=None)
+view_menu.add_command(label='Pick a day', command=None)
+# Week View SubMenu
+week_menu = Menu(view_menu, tearoff=False)
+view_menu.add_cascade(label='Week', menu=week_menu)
+# Year View SubMenu
+year_menu = Menu(view_menu, tearoff=False)
+view_menu.add_cascade(label='Year', menu=year_menu)
+
+# Contact Us Menu
+contact_menu = Menu(sala_menu, tearoff=False)
+sala_menu.add_cascade(label='Contact Us', menu=contact_menu)
+contact_menu.add_command(label='Email', command=None, state='disabled')
+contact_menu.add_command(label='info@am6.tech', command=None)
+contact_menu.add_separator()
+contact_menu.add_command(label='Website', command=opening_website, state='disabled')
+contact_menu.add_command(label='am6.tech', command=opening_website)
+contact_menu.add_separator()
+contact_menu.add_command(label='Facbook Page', command=opening_fb, state='disabled')
+contact_menu.add_command(label='facebook.com/AM6Page', command=opening_fb)
+contact_menu.add_separator()
+contact_menu.add_command(label='Phone Number', command=None, state='disabled')
+contact_menu.add_command(label='+20 155 305 7088', command=None)
+contact_menu.add_command(label='Whatsapp', command=opening_whatsapp)
+contact_menu.add_command(label='Telegram', command=opening_telegram)
+contact_menu.add_command(label='Call Us', command=calling_us)
+
 
 # db
 day = datetime.date.today()
@@ -82,12 +146,17 @@ frame1.pack(padx=30, pady=30)
 
 
 # list of checkbuttons
-c1 = Checkbutton(frame1,text="Fajr | الفجر", variable=var1).pack()
-c2 = Checkbutton(frame1,text="Dhohr | الظهر", variable=var2).pack()
-c3 = Checkbutton(frame1,text="Asr | العصر", variable=var3).pack()
-c4 = Checkbutton(frame1,text="Maghreb | المغرب", variable=var4).pack()
-c5 = Checkbutton(frame1,text="Isha | العشاء", variable=var5).pack()
+c1 = Checkbutton(frame1,text="Fajr | الفجر", variable=var1)
+c2 = Checkbutton(frame1,text="Dhohr | الظهر", variable=var2)
+c3 = Checkbutton(frame1,text="Asr | العصر", variable=var3)
+c4 = Checkbutton(frame1,text="Maghreb | المغرب", variable=var4)
+c5 = Checkbutton(frame1,text="Isha | العشاء", variable=var5)
 
+c1.pack()
+c2.pack()
+c3.pack()
+c4.pack()
+c5.pack()
 
 def submit():
     now = datetime.datetime.now()
@@ -109,8 +178,8 @@ def submit():
     messagebox.showinfo('Submit', 'تم الحفظ')
 
 # submit button
-submit = Button(frame1, text='Submit | حفظ', command=submit).pack()
-
+submit = Button(frame1, text='Submit | حفظ', command=submit)
+submit.pack()
 
 db.close()
 # maitianing the app open
